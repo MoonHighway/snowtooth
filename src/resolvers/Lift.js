3961c7b00aa4de0aaec8f4d451f0c22516e99677
Bobby Mathews <mathews.bobby@bcg.com>
2019-10-29 13:36:51 -0700

    Add a metric param for elevationGain

--- src/resolvers/Lift.js
+++ src/resolvers/Lift.js
@@ -1,4 +1,18 @@
 export default {
   trailAccess: (parent, args, { trails }) =>
-    parent.trails.map(id => trails.find(t => id === t.id))
+    parent.trails.map(id => trails.find(t => id === t.id)),
+  elevationGain: (parent, { metric }) => {
+    const { elevationGain } = parent;
+    if (typeof elevationGain === "number") {
+      switch (metric) {
+        case "FEET":
+          return elevationGain; // NOTE: Assumes existing data is in feet.
+        case "YARDS":
+          return Math.round(elevationGain / 3.0);
+        default:
+          throw new Error("Unexpected metric type.");
+      }
+    }
+    return elevationGain;
+  }
 };
