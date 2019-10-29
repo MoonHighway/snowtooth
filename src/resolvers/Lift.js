dd3f972d0d444d1d43074e053e652210de87bbd4
Jiri Barton <jbar@tele3.cz>
2019-10-29 13:49:21 -0700

    added isOperational to lifts

--- src/resolvers/Lift.js
+++ src/resolvers/Lift.js
@@ -1,4 +1,5 @@
 export default {
   trailAccess: (parent, args, { trails }) =>
-    parent.trails.map(id => trails.find(t => id === t.id))
+    parent.trails.map(id => trails.find(t => id === t.id)),
+  isOperational: (parent, args, { trails }) => parent.status == "OPEN"
 };
