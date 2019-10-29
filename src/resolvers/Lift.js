fa597dd89cd881c1fa362eeb96998c0c51207049
Gianni Maize <gianni.maize@fictiv.com>
2019-10-29 13:18:54 -0700

    blah

--- src/resolvers/Lift.js
+++ src/resolvers/Lift.js
@@ -1,4 +1,12 @@
 export default {
   trailAccess: (parent, args, { trails }) =>
-    parent.trails.map(id => trails.find(t => id === t.id))
+    parent.trails.map(id => trails.find(t => id === t.id)),
+  trailNames: (parent, args, { trails }) =>
+  	parent.trails.map(id => {
+  		const trail = trails.find(t => id === t.id)
+  		if (trail) {
+  			return trail.name
+  		}
+  		return null
+  	})
 };
