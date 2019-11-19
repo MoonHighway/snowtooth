eb95f1eae5d01934df2ee3aa419995e526bdd102
Satoshi Scott Iwako <iwakoscott@gmail.com>
2019-11-19 09:51:22 -0800

    deprecate gnar

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,6 @@
 export default {
   sweet: () => "sweet",
+  gnar: () => "gnarly",
   allLifts: (parent, { status }, { lifts }) =>
     !status ? lifts : lifts.filter(lift => lift.status === status),
   allTrails: (parent, { status }, { trails }) =>
